3b56eb096a1853cbe7dba5d8489bd0346a00f499
Irmak_Tekin <irmaktekin97@gmail.com>
2022-12-27 16:47:32 +0300

    Spring boot project

---
+++ learn-spring-boot/learn-spring-boot/src/main/java/com/irmaktekin/learnspringboot/CourseController.java
@@ -0,0 +1,23 @@
+package com.irmaktekin.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+	
+	@RequestMapping("/courses")
+	public List <Course> retrieveAllCourse(){
+		return Arrays.asList(
+					new Course(1,"Learn AWS", "irmaktekin"),
+					new Course(1,"Learn DevOps", "irmaktekin")
+
+				);
+		
+	}
+	
+
+}
